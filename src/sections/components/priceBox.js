e43483f48e2a233e85874fc28342d126878e04db
icarus <ellishogan95@gmail.com>
2020-10-16 01:45:43 -0400

    update

--- src/sections/components/priceBox.js
+++ src/sections/components/priceBox.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-export default class PriceBox extends React.Component {
-
-	  render(){
-		  return (
-				<div className="price-card p-3">
-                    <h5 id="pages" className={this.props.pages === 0 ? "d-none" : "d-block"}>
-                        Basic Site Needs: <span className="float-right">$ {this.props.pages}</span>
-                    </h5>
-                    <h5 id="design" className={this.props.design === 0 ? "d-none" : "d-block"}>
-                        Dynamic Design: <span className="float-right">$ {this.props.design}</span>
-                    </h5>
-                    <h5 id="programming" className={this.props.programming === 0 ? "d-none" : "d-block"}>
-                        Programming: <span className="float-right">$ {this.props.programming}</span>
-                    </h5>
-					<hr />
-					<h4 className="px-3 bold">
-						Subtotal:  <span className="float-right">$ {this.props.pages + this.props.design + this.props.programming}</span>
-					</h4>				
-				</div>
-		  );
-	  }
+const PriceBox = (props) =>  {
+	return (
+		<div className="price-card p-3">
+			<h5 id="pages" className={props.pages === 0 ? "d-none" : "d-block"}>
+					Basic Site Needs: <span className="float-right">$ {props.pages}</span>
+			</h5>
+			<h5 id="design" className={props.design === 0 ? "d-none" : "d-block"}>
+					Dynamic Design: <span className="float-right">$ {props.design}</span>
+			</h5>
+			<h5 id="programming" className={props.programming === 0 ? "d-none" : "d-block"}>
+					Programming: <span className="float-right">$ {props.programming}</span>
+			</h5>
+			<hr />
+			<h4 className="px-3 bold">
+				Subtotal:  <span className="float-right">$ {props.pages + props.design + props.programming}</span>
+			</h4>				
+		</div>
+	);
+}
 	
-  }
+export default PriceBox
